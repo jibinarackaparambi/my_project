f23ab27dcf188435c339a8639be7b073370d61a2
jibinarackaparambil <jibinarackaparambil@gmail.com>
2025-12-03 18:06:16 +0530

    New status added: PENDING

--- api/models.py
+++ api/models.py
@@ -6,12 +6,13 @@ class Task(models.Model):
         TODO = "TODO", "To Do"
         INPROGRES = "INPROGRES", "In Progres"
         COMPLETED = "COMPLETED", "Completed"
+        PENDING = "PENDING", "Pending"
 
     title = models.CharField(max_length=30)
     description = models.TextField()
     status = models.CharField(
         max_length=20,
         choices=Status.choices,
-        default=Status.TODO,
+        default=Status.PENDING,
     )
     created_at = models.DateTimeField(auto_now_add=True)
